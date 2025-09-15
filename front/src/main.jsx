0f50fbbdf4a19bc9e0773e6ed8c164aa26d041d6
DESKTOP-8KKE7FU\soldesk <milo.lee1605@gmail.com>
2025-09-15 10:51:46 +0900

    test2

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom';
+import {BrowserRouter, HashRouter} from 'react-router-dom';
 import App from './App.jsx'
 import './index.css'
 import {GoogleOAuthProvider} from "@react-oauth/google";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
         <GoogleOAuthProvider clientId="300401840037-rnfvf6ucua95aufhljvt5g5orlbkrp4m.apps.googleusercontent.com">
             <App />
         </GoogleOAuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
 
